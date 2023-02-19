b069c0010263928739632ce6f10e707b247afd8c
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-02-19 18:05:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ uint64_t nPrime(uint64_t n) {
             n--;
         a++;
     }
-	return a-1;
+    return a-1;
 }
 
 uint64_t nextPrime(uint64_t value) {
@@ -27,12 +27,12 @@ uint64_t nextPrime(uint64_t value) {
         if (checkPrime(i)) {
             return i;
             break;
-		}
+        }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-	for (uint64_t i = 2; i < hbound; i++)
+    for (uint64_t i = 2; i < hbound; i++)
         if (checkPrime(i))
             sum += i;
     return sum;
